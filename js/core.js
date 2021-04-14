f058c04e42247650be50fcadba7e8ed9ad4fab44
apt-get2Update <apt-get2Update@users.noreply.github.com>
2021-04-14 12:54:12 +0000

    Automated deployment: Wed Apr 14 12:54:12 UTC 2021 69e703b7b6e85e612841bc62d0ff35e2a55208b1

--- js/core.js
+++ js/core.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function * generatData() { 
-    const urls = ["http://run.mocky.io/v3/6f7a76ed-d6f5-4b54-be23-bf9a141c982a","http://run.mocky.io/v3/07316365-b8d2-4574-9bc1-22b17b054e3b","http://run.mocky.io/v3/1c56213e-1191-4b47-a54f-066736165ff3"];
+    const urls = ["https://run.mocky.io/v3/6f7a76ed-d6f5-4b54-be23-bf9a141c982a","https://run.mocky.io/v3/07316365-b8d2-4574-9bc1-22b17b054e3b","https://run.mocky.io/v3/1c56213e-1191-4b47-a54f-066736165ff3"];
     let i = 0;
     while (true) {
         yield fetch(urls[i%3], {
